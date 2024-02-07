6af63b5cf0fdd3c7bfb512a621ad320d7f2cc0b6
manvendra9752 <manvendrachouhan11@gmail.com>
2024-02-07 21:48:17 +0530

    secondcommit

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -36,6 +36,7 @@ function MyWork() {
                   href="https://main--harmonious-conkies-c685e7.netlify.app/"
                   className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
                   target="_blank"
+                  rel="noreferrer"
                 >
                   <BsLaptop /> Watch Live
                 </a>
@@ -43,48 +44,51 @@ function MyWork() {
                   href="https://github.com/manvendra9752/WheatherForcast"
                   className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
                   target="_blank"
+                  rel="noreferrer"
                 >
                   <AiFillGithub /> Source Code
                 </a>
               </div>
             </div>
 
-            <div className="row">
-              <div className="col-md-4 text-center my-2">
-                <div
-                  id="carouselExampleControls1"
-                  className="carousel slide my-1"
-                  data-bs-ride="carousel"
-                >
-                  <div className="carousel-inner">
-                    <div className="carousel-item active">
-                      <img
-                        src="../portfolio-images/towerofhanoi.png"
-                        className="d-block w-100"
-                        alt="..."
-                      />
-                    </div>
+            <div className="col-md-4 text-center my-2">
+              <div
+                id="carouselExampleControls1"
+                className="carousel slide my-1"
+                data-bs-ride="carousel"
+              >
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img
+                      src="../portfolio-images/towerofhanoi.png"
+                      className="d-block w-100"
+                      alt="..."
+                    />
                   </div>
                 </div>
-                <h5 className="ff-jose my-1">Tower of hanoi</h5>
-                <div className="row d-flex justify-content-center">
-                  <a
-                    href="https://tower-of-hanoi-alpha.vercel.app/"
-                    className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
-                    target="_blank"
-                  >
-                    <BsLaptop /> Watch Live
-                  </a>
-                  <a
-                    href="https://github.com/manvendra9752/towerofhanoi"
-                    className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
-                    target="_blank"
-                  >
-                    <AiFillGithub /> Source Code
-                  </a>
-                </div>
+              </div>
+              <h5 className="ff-jose my-1">Tower of Hanoi</h5>
+              <div className="row d-flex justify-content-center">
+                <a
+                  href="https://tower-of-hanoi-alpha.vercel.app/"
+                  className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsLaptop /> Watch Live
+                </a>
+                <a
+                  href="https://github.com/manvendra9752/towerofhanoi"
+                  className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiFillGithub /> Source Code
+                </a>
               </div>
             </div>
+          </div>
+          <div className="row">
             <div className="col-md-4 text-center my-2">
               <div
                 id="carouselExampleControls2"
@@ -100,61 +104,62 @@ function MyWork() {
                     />
                   </div>
                 </div>
-                <div>
-                  <h5 className="ff-jose my-1">Platform design</h5>
-                  <div className="row d-flex justify-content-center">
-                    <a
-                      href="https://peppy-sunburst-effa6e.netlify.app"
-                      className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
-                      target="_blank"
-                    >
-                      <BsLaptop /> Watch Live
-                    </a>
-                    <a
-                      href="https://github.com/manvendra9752/Msctechnical"
-                      className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
-                      target="_blank"
-                    >
-                      <AiFillGithub /> Source Code
-                    </a>
+              </div>
+              <h5 className="ff-jose my-1">Platform design</h5>
+              <div className="row d-flex justify-content-center">
+                <a
+                  href="https://peppy-sunburst-effa6e.netlify.app"
+                  className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsLaptop /> Watch Live
+                </a>
+                <a
+                  href="https://github.com/manvendra9752/Msctechnical"
+                  className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiFillGithub /> Source Code
+                </a>
+              </div>
+            </div>
+
+            <div className="col-md-4 text-center my-2">
+              <div
+                id="carouselExampleControls2"
+                className="carousel slide my-1"
+                data-bs-ride="carousel"
+              >
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img
+                      src="../portfolio-images/countdown.png"
+                      className="d-block w-100"
+                      alt="..."
+                    />
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="col-md-4 text-center my-2">
-            <div
-              id="carouselExampleControls2"
-              className="carousel slide my-1"
-              data-bs-ride="carousel"
-            >
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <img
-                    src="../portfolio-images/countdown.png"
-                    className="d-block w-100"
-                    alt="..."
-                  />
-                </div>
-              </div>
-              <div>
-                <h5 className="ff-jose my-1">Count Down with Stopwatch</h5>
-                <div className="row d-flex justify-content-center">
-                  <a
-                    href="https://eventmanager-beta.vercel.app/"
-                    className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
-                    target="_blank"
-                  >
-                    <BsLaptop /> Watch Live
-                  </a>
-                  <a
-                    href="https://github.com/manvendra9752/eventmanager"
-                    className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
-                    target="_blank"
-                  >
-                    <AiFillGithub /> Source Code
-                  </a>
-                </div>
+              <h5 className="ff-jose my-1">Count Down with Stopwatch</h5>
+              <div className="row d-flex justify-content-center">
+                <a
+                  href="https://eventmanager-beta.vercel.app/"
+                  className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsLaptop /> Watch Live
+                </a>
+                <a
+                  href="https://github.com/manvendra9752/eventmanager"
+                  className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiFillGithub /> Source Code
+                </a>
               </div>
             </div>
           </div>
